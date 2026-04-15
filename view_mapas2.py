3d06628ea99d9ac7b6b19c4ce2951b60e3a0cd22
paola-89 <132857626+paola-89@users.noreply.github.com>
2026-04-14 22:08:07 -0600

    Add files via upload

--- view_mapas2.py
+++ view_mapas2.py
@@ -1,32 +1,45 @@
+
 import streamlit as st
 import os
 from PIL import Image
 
-# 📁 Ruta a tus mapas
-IMG_DIR = "output/VP_ae/mapas"
-
 st.set_page_config(layout="wide")
+
 st.title("Mapas Demográficos 🗺️")
 
-# 🎛️ Controles
-sexo = st.selectbox("Selecciona sexo:", ["Hombre", "Mujer"])
-anio = st.selectbox("Selecciona año:", ["1990", "1995", "2000", "2005", "2010", "2020"])
+# 📁 Rutas
+IMG_DIRS = {
+    "VP": "output/VP_ae/mapas",
+    "VC": "output/VC_ae/mapas"
+}
 
-# 📂 Obtener archivos
+# 🎛️ Controles
+tipo = st.selectbox("Tipo de vivienda:", ["VP", "VC"])
+sexo = st.selectbox("Sexo:", ["Hombre", "Mujer"])
+anio = st.selectbox("Año:", ["1990", "1995", "2000", "2005", "2010", "2020"])
+
+IMG_DIR = IMG_DIRS[tipo]
+
+#  Obtener archivos
 files = [
     f for f in os.listdir(IMG_DIR)
     if sexo in f and anio in f
 ]
 
+# ordenar por edad
 orden = ["95-99", "100-104", "105-109", "110-114", "115+"]
-files = sorted(files, key=lambda x: next((i for i, e in enumerate(orden) if e in x), 99))
 
-# 🖼️ Mostrar mapas en grid
+files = sorted(
+    files,
+    key=lambda x: next((i for i, e in enumerate(orden) if e in x), 99)
+)
+
+# Mostrar mapas 
 cols = st.columns(2)
 
 for i, file in enumerate(files):
     img_path = os.path.join(IMG_DIR, file)
-    image = Image.open(img_path)
     
     with cols[i % 2]:
-        st.image(image, caption = file.replace("mapa_", "").replace(".png", ""), use_container_width=True)
+        caption = file.replace("mapa_", "").replace(".png", "")
+        st.image(img_path, caption=caption, use_container_width=True)
